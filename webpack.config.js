73e50003ad5891374f01bbf1770cec525933b600
vladovoev <vladovoev@yahoo.co.uk>
2018-07-16 15:32:24 +0300

    b

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,42 @@ const path = require('path');
 const webpack = require('webpack');
 
 const config = {
-    resolve:{
-        modules:[
-            path.resolve('./lib'),
-            path.resolve('./node_modules'),
-        ]
-    },
-    entry: [
-        'babel-polyfill',
-        './lib/renderers/dom.js'
+  resolve: {
+    modules: [path.resolve('./lib'), path.resolve('./node_modules')]
+  },
+  entry: {
+    vendor: [
+      'babel-polyfill',
+      'react',
+      'react-dom',
+      'prop-types',
+      'axios',
+      'lodash.debounce',
+      'lodash.pickby'
     ],
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.(js|jsx)$/,
-            use: { loader: 'babel-loader' },
-            exclude: /node_modules/
-        }]
-    },
+    app: ['./lib/renderers/dom.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'env', 'stage-2']
+        }
+      }
+    }]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    })
+  ]
 };
 
 module.exports = config;
